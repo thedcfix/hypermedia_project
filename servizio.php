6afb23eaea987284e4329dc16148e1a3b6c2d175
Davide Cattaneo <davide.cattaneo.it@gmail.com>
2016-07-16 21:15:36 +0200

    fatto servizio dinamico, manca solo il carousel dei prodotti collegati

--- servizio.php
+++ servizio.php
@@ -1,30 +1,45 @@
 <?php 
 
 	if(isset($_GET['id'])) {
-		$category = $_GET['id'];
+		$id = $_GET['id'];
 		$conn = mysqli_connect("localhost", "root", "");
     	mysqli_select_db($conn, "hyp_db");
-    	$res = mysqli_query($conn, "select * from servizi where id = '$id'");   
+    	$stringa = "select * from servizi where id = '$id'";
+    	$ris = $conn->query($stringa);
+    	$row = mysqli_fetch_array($ris);
+        $id = $row['id'];
+        $imma = $row["img"];
 
     	$doc = new DOMDocument();
     	libxml_use_internal_errors(true);
-    	$doc->loadHTMLFile("assistenza_servizi.html");
-    	
-    	$elenco_servizi_assistenza = $doc->getElementById('contenuto');
-        $frammento =$doc->createDocumentFragment();
+    	$doc->loadHTMLFile("servizio_template.html");
+    	$frammento =$doc->createDocumentFragment();
 
-         while($row = mysqli_fetch_array($res)){
-             $aggiunta = '<div class="col-sm-4" align="center" style="float:left"><a href="assistance.php?id='.$row["id"].'"><img src="'.$row["img"].'" style="width:160px; height:160px"></img><p>'.$row["servizio"].'<br/></p></a></div>';
-             // <img src="'.$img["img"].'" style="width:230px; height:230px"></img>
-             $frammento->appendXML($aggiunta);
-             $elenco_servizi_assistenza->appendChild($frammento);
-         }
 
-        $cat = $doc->getElementById('categoria');
-        $frammento->appendXML($category);
-        $cat->appendChild($frammento);
 
-        echo $doc->saveHTML();
+        //$bread = $doc->getElementById('immagine');
+       // $frammento->appendXML('<a href="products.html?type='.$row['tipologia'].'">'.$row['tipologia'].'</a>');
+        //$bread->appendChild($frammento);
+
+    	$img = $doc->getElementById('immagine');
+    	$frammento->appendXML('<img src="'.$row["img"].'"></img><br/>');
+    	$img->appendChild($frammento);
+
+		$offerta = $doc->getElementById("offerta");
+    	$frammento->appendXML("<br/>".$row["offerta"]."<br/><br/>");
+    	$offerta->appendChild($frammento);
+
+		$costi = $doc->getElementById("costi");
+    	$frammento->appendXML("<br/>".$row["dettagli_costi"]."<br/><br/>");
+    	$costi->appendChild($frammento);
+
+		// prendo tutti i device compatibili col servizio
+		$prodotti = "select prodotti.id from servizi join prodotti_servizi on servizi.id = prodotti_servizi.id_servizio join prodotti on prodotti_servizi.id_prodotto = prodotti.id where servizi.id = $id";
+		$res = $conn->query($prodotti);
+
+		// qui bisogna inserire le cose nel carousel
+        
+    	echo $doc->saveHTML();
     	libxml_clear_errors();
 	}
 	else{
