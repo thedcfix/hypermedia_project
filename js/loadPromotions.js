5396d9466b1231966e8b3db5d0429de35261fcd1
CpxeE <fracerri94@gmail.com>
2016-07-18 19:44:29 +0200

    Aggiunto filtro promozioni

--- js/loadPromotions.js
+++ js/loadPromotions.js
@@ -1,4 +1,26 @@
 $("document").ready(function(){
+
+appendAll();
+
+$('.resetfiltri').click(function() {
+    removeAll();
+    appendAll();
+});
+
+$(".150").click(function() {
+    cerca(0,150);
+});
+
+$(".150e450").click(function() {
+    cerca(150,450);
+});
+
+$(".450").click(function() {
+    cerca(450,10000);
+});
+});
+
+function appendAll(){
     $.ajax({
         method: "POST", 
         crossDomain: true,
@@ -7,10 +29,42 @@ $("document").ready(function(){
             console.log("riuscito!");
             console.log(response);
             $("#content").append(response);
+            $("#contentxs").append(response);
         },
         error: function(request,error){
             console.log(request+":"+error);
         }
     });
     return false;
-});
+};
+
+function removeAll() {
+    $("#content").empty();
+    $("#contentxs").empty();
+};
+
+function cerca(min,max){
+    removeAll();
+    appendMinMax(min,max);
+
+};
+
+function appendMinMax(min,max){
+
+    $.ajax({
+        method: "POST", 
+        crossDomain: true,
+        url: "http://reyze.altervista.org/php/oracle.php",
+        data : { min : min , max : max , method : 'in_evidenza_min_max' },
+        success: function(response) {
+            console.log("riuscito!");
+            console.log(response);
+            $("#content").append(response);
+            $("#contentxs").append(response);
+        },
+        error: function(request,error){
+            console.log(request+":"+error);
+        }
+    });    
+
+};
--- php/oracle.php
+++ php/oracle.php
@@ -30,6 +30,19 @@ switch ($_POST['method']) {
         }
         break;
 
+    case 'in_evidenza_min_max':
+        $conn = mysqli_connect("localhost", "root", "");
+        mysqli_select_db($conn, "my_reyze");
+        $res = productsShowMinMaxEvid($conn,$_POST['min'],$_POST['max']);
+        while($row = mysqli_fetch_array($res)) {
+            $var = $row['id'];
+            $imma = mysqli_query($conn, "select img from immagini where disp_id = $var");
+            $img =  mysqli_fetch_array($imma);
+            echo '<div class="col-sm-4" align="center" style="float:left"><a href="http://reyze.altervista.org/device.php?id='.$row["id"].'""><img src="'.$img["img"].'" style="width:230px; height:230px"><p>'.$row["marca"].'<br/>'.$row["nome"].'</p></a></div>';
+        }
+        break;
+
+
     default:
         echo "Non ho questo metodo!!!";
         break;
@@ -84,6 +97,14 @@ function productsShowMinMax($type,$conn,$min,$max) {
     else {
         $res = mysqli_query($conn, "select * from prodotti where outlet && prezzo < $max && prezzo > $min order by Prezzo DESC");
     }
+    return $res;
+
+}
+
+function productsShowMinMaxEvid($conn,$min,$max) {
+
+    $res = mysqli_query($conn, "select * from prodotti where in_evidenza = true && prezzo < $max && prezzo > $min order by Prezzo DESC");
+
     return $res;
 
 }
